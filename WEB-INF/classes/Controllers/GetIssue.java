ba7489f404a20cd9ade62865a339c01ab146f787
Brendan <brendan.rogerz@gmail.com>
2016-10-20 17:52:20 +1100

    Added servlet template code into the controllers

--- WEB-INF/classes/Controllers/GetIssue.java
+++ WEB-INF/classes/Controllers/GetIssue.java
@@ -1,7 +1,27 @@
 package Controllers;
 
+import javax.servlet.*;
+import java.io.IOException;
+
 /**
  * Created by Brendan on 19/10/2016.
  */
-public class GetIssue {
+@WebServlet(urlPatterns = {"/Issue"})
+public class GetIssue extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //copied this shit from uonSales
+
+        // The user has come here by mistake.
+        // It is possible that they have tried logging in after trying to buy an item and being shown the error message
+        // Is this a valid assumption? if not you might need to do something else here
+        response.sendRedirect("index.jsp");
+    }
+
 }
