7165e14f4a21fefbfeb16eda5eb704647c12d2f6
Daniel McKinnell <daniel.mckinnell43@gmail.com>
2016-10-31 19:44:08 +1100

    added some sql shit to knowledgebase and issue report

--- WEB-INF/classes/Controllers/IssueReport.java
+++ WEB-INF/classes/Controllers/IssueReport.java
@@ -3,57 +3,78 @@ package Controllers;
 import Models.Issue;
 import Models.User;
 
+import javax.activation.DataSource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
+import javax.sql.*;
 import java.util.Date;
 
 /**
  * Created by Brendan on 19/10/2016.
  */
 @WebServlet(urlPatterns = {"/ReportIssue"})
-public class IssueReport extends HttpServlet{
+public class IssueReport extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         User user = (User) request.getSession().getAttribute("user");
 
+        try {
+            javax.sql.DataSource datasource = (javax.sql.DataSource) new
+                    InitialContext().lookup("java:/comp/env/SENG2050");
 
-        Issue issue = new Issue();
-        /*
-        issue.setUser(user.getUsername());
-        issue.setContent(request.getParameter("content"));
-        issue.setStatus("whateverTheStartingStatusIs");
-        issue.setCategory(request.getParameter("category"));
-        issue.setTitle(request.getParameter("title"));
-        issue.setDescription(request.getParameter("description"));
-        issue.setReportedDateTime(new Date());
+            Connection connection = datasource.getConnection();
 
-        issue.setIssueID(0); //need to set it to the total number of issues + 1
-        */
+            String statement = "SELECT COUNT(*) FROM Issue AS numOfIssues;";
+            PreparedStatement s = connection.prepareStatement(statement);
+            ResultSet rs = s.executeQuery();
 
-        issue.setIssueID(0); //till we get the form working
-        issue.setStatus("status");
-        issue.setCategory("category");
-        issue.setReportedDateTime(new Date());
-        issue.setUser("user");
-        //add this shit to sql database
+            int numOfIssues = rs.getInt("numOfIssues");
+
+            /*
+            Issue issue = new Issue();
+
+            issue.setUser(user.getUsername());
+            issue.setContent(request.getParameter("content"));
+            issue.setStatus("whateverTheStartingStatusIs");
+            issue.setCategory(request.getParameter("category"));
+            issue.setTitle(request.getParameter("title"));
+            issue.setDescription(request.getParameter("description"));
+            issue.setReportedDateTime(new Date());
+
+            issue.setIssueID(numOfIssues + 1); //need to set it to the total number of issues + 1
+            */
+
+            ///////////////////////needs rest of attributes ///////////////////////////
+
+            statement = "INSERT INTO Issue(issueID, content, state....) VALUES (?, ?, ?, .........)";
+            s = connection.prepareStatement(statement);
+            s.setInt(1, numOfIssues + 1);
+            s.setString(2, request.getParameter("content"));
+            s.setString(3, "whateverTheStartingStatusIs");
+            ////////////////////////rest of the attributes ////////////////////////
+            s.executeUpdate();
 
 
+        } catch (Exception e){
+            ////////////////////// set error tag in the session //////////////////////////////////////////
+        }
+
+        //////////////////redirect/////////////////////////
     }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //copied this shit from uonSales
 
-        // The user has come here by mistake.
-        // It is possible that they have tried logging in after trying to buy an item and being shown the error message
-        // Is this a valid assumption? if not you might need to do something else here
-        response.sendRedirect("index.jsp");
+        doPost(request, response);
     }
 
 }
