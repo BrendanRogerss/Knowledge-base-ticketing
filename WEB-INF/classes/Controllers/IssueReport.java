d2cf8c69d6ee008fcd9f19b23273a33400dfdd88
Brendan <brendan.rogerz@gmail.com>
2016-10-31 18:17:38 +1100

    added code to take in input from a form and make an issue in IssueReport

--- WEB-INF/classes/Controllers/IssueReport.java
+++ WEB-INF/classes/Controllers/IssueReport.java
@@ -1,11 +1,16 @@
 package Controllers;
 
+import Models.Issue;
+import Models.User;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+
 /**
  * Created by Brendan on 19/10/2016.
  */
@@ -14,6 +19,30 @@ public class IssueReport extends HttpServlet{
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        User user = (User) request.getSession().getAttribute("user");
+
+
+        Issue issue = new Issue();
+        /*
+        issue.setUser(user.getUsername());
+        issue.setContent(request.getParameter("content"));
+        issue.setStatus("whateverTheStartingStatusIs");
+        issue.setCategory(request.getParameter("category"));
+        issue.setTitle(request.getParameter("title"));
+        issue.setDescription(request.getParameter("description"));
+        issue.setReportedDateTime(new Date());
+
+        issue.setIssueID(0); //need to set it to the total number of issues + 1
+        */
+
+        issue.setIssueID(0); //till we get the form working
+        issue.setStatus("status");
+        issue.setCategory("category");
+        issue.setReportedDateTime(new Date());
+        issue.setUser("user");
+        //add this shit to sql database
+
 
     }
 
